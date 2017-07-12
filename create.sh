e7a93aded2738abf2740737fc75c8126db76a609
Russell <ruzzll@hotmail.com>
2017-07-12 18:57:44 -0500

    fixes

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 filename=cert-`date +%Y`
 
-openssl req -config  config.conf -new -newkey rsa:2048 -nodes -keyout ${filename}.key -out ${filename}.csr
+openssl req -config  config.conf -new -newkey rsa:2048 -nodes -keyout ${filename}.key -out ${filename}.csr8 -passout pass:foobar
